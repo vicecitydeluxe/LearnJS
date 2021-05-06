d8f77d25bdfa390493b80f1511baa94cd645dec5
vicecitydeluxe <83658260+vicecitydeluxe@users.noreply.github.com>
2021-05-06 20:31:08 +0300

    Add files via upload

---
+++ Learn JS/2.4/script1.js
@@ -0,0 +1,5 @@
+let admin;
+let name='John' ;
+admin=name; 
+name='John';
+alert (admin); //Выпадает просто "Джон", т.к. цель проверить правильность присваиваний я так понял?
---
+++ Learn JS/2.5/script3.js
@@ -0,0 +1,4 @@
+let name = "Ilya";
+alert( `hello ${1}` ); // Выпадает hello 1
+alert( `hello ${"name"}` ); // Выпадает hello name
+alert( `hello ${name}` ); // Выпадает hello Ilya
