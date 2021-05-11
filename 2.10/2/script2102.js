0af0d5dbd68d374b833db70b712150144b11c92e
vicecitydeluxe <vikvas95@gmail.com>
2021-05-11 18:33:59 +0300

    ok

---
+++ 2.10/2/script2102.js
@@ -0,0 +1,6 @@
+let x = prompt('Какое «официальное» название JavaScript?', '');
+if (x == "ECMAScript") {
+alert( 'Верно!' );
+} else {
+alert( 'Не знаете? ECMAScript!' ); 
+}
---
+++ 2.10/3/script2103.js
@@ -0,0 +1,9 @@
+let x = prompt('Введите любое число', '');
+
+if (x >0) {
+  alert( '1' );
+} else if (x < 0) {
+  alert( '-1' );
+} else {
+  alert( '0' );
+}
