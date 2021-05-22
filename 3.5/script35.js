28ce02a697b7268934e878ea6ca44eb4e87bc9a1
vicecitydeluxe <vikvas95@gmail.com>
2021-05-22 15:31:57 +0300

    Added tasks from 3.0 to test pull request feature

---
+++ 3.5/script35.js
@@ -0,0 +1,15 @@
+// Что не так в нижеприведённом тесте функции pow?
+
+// it("Возводит x в степень n", function() {
+//   let x = 5;
+
+//   let result = x;
+//   assert.equal(pow(x, 1), result);
+
+//   result *= x;
+//   assert.equal(pow(x, 2), result);
+
+//   result *= x;
+//   assert.equal(pow(x, 3), result);
+// }); 
+//^это предлагаю сделать вместе
