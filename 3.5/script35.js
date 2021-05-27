991035a200df60dfb363ac8a994c443bed308f4d
vicecitydeluxe <vikvas95@gmail.com>
2021-05-27 21:46:46 +0300

    Finished 3.0 module and started 4.0

--- 3.5/script35.js
+++ 3.5/script35.js
@@ -1,15 +1,33 @@
-// Что не так в нижеприведённом тесте функции pow?
+"use strict";
+/*
+Что не так с этим тестом?
+Что не так в нижеприведённом тесте функции pow?
 
-// it("Возводит x в степень n", function() {
-//   let x = 5;
+it("Возводит x в степень n", function() {
+  let x = 5;
 
-//   let result = x;
-//   assert.equal(pow(x, 1), result);
+  let result = x;
+  assert.equal(pow(x, 1), result);
 
-//   result *= x;
-//   assert.equal(pow(x, 2), result);
+  result *= x;
+  assert.equal(pow(x, 2), result);
 
-//   result *= x;
-//   assert.equal(pow(x, 3), result);
-// }); 
-//^это предлагаю сделать вместе
+  result *= x;
+  assert.equal(pow(x, 3), result);
+}); 
+*/
+describe("Возводит x в степень n", function () {
+  it("5 в степени 1 будет 5", function () {
+    assert.equal(pow(5, 1), 5);
+  });
+//мы выделили каждый конкретный тест в отдельные блоки it
+  it("5 в степени 2 будет 25", function () {
+    assert.equal(pow(5, 2), 25);
+  });
+//вместо переходящего Х как в ДАНО, мы уточняем переменные
+  it("5 в степени 3 будет 125", function () {
+    assert.equal(pow(5, 3), 125);
+  });
+});/* в результате тест работает корректнее,
+    песочница даст ощутить разницу, 
+   там будет 3 отдельые строки на каждый тест*/
---
+++ 4.1/2/script412.js
@@ -0,0 +1,9 @@
+"use strict";
+/*Напишите функцию isEmpty(obj), которая возвращает true, если у объекта нет свойств, иначе false.*/
+
+function isEmpty(obj) {
+  for (key in obj) {
+    return false;
+  }
+  return true;
+}
